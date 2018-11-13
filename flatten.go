28cac46ff530a0ba2985e8e96c498d988355944f
Cheng Li <cli99@illinois.edu>
2018-11-13 05:46:38 -0600

    fix

--- flatten.go
+++ flatten.go
@@ -1,5 +1,30 @@
 package utils
 
+import (
+	"github.com/spf13/cast"
+)
+
+func FlattenFloat32(f interface{}) []float32 {
+
+	output := []float32{}
+
+	switch e := f.(type) {
+	case [][]float32:
+		for _, v := range e {
+			output = append(output, v...)
+		}
+	case []interface{}:
+		for _, v := range e {
+			output = append(output, FlattenFloat32(v)...)
+		}
+	case float32:
+		output = append(output, e)
+	default:
+		output = append(output, cast.ToFloat32(e))
+	}
+	return output
+}
+
 func Flatten(f interface{}) []interface{} {
 
 	output := []interface{}{}
