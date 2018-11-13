4f4bcfa3ccfa76a563dead2cf999310718bf9461
Cheng Li <cli99@illinois.edu>
2018-11-13 07:02:30 -0600

    not wokring

--- flatten.go
+++ flatten.go
@@ -1,80 +1,32 @@
 package utils
 
-import (
-	"fmt"
-	"reflect"
+import "github.com/pkg/errors"
 
-	"github.com/spf13/cast"
-)
-
-// casts an interface to a []float32 type.
-func Tofloat32SliceE(i interface{}) ([]float32, error) {
-	if i == nil {
-		return []float32{}, fmt.Errorf("unable to cast %#v of type %T to []float32", i, i)
-	}
-
-	switch v := i.(type) {
-	case []float32:
-		return v, nil
-	}
-
-	kind := reflect.TypeOf(i).Kind()
-	switch kind {
-	case reflect.Slice, reflect.Array:
-		s := reflect.ValueOf(i)
-		a := make([]float32, s.Len())
-		for j := 0; j < s.Len(); j++ {
-			val, err := cast.Tofloat32E(s.Index(j).Interface())
-			if err != nil {
-				return []int{}, fmt.Errorf("unable to cast %#v of type %T to []float32", i, i)
-			}
-			a[j] = val
-		}
-		return a, nil
-	default:
-		return []int{}, fmt.Errorf("unable to cast %#v of type %T to []float32", i, i)
-	}
-}
-
-// func FlattenFloat32(f interface{}) []float32 {
-
-// 	output := []float32{}
-
-// 	switch e := f.(type) {
-// 	case [][]float32:
-// 		for _, v := range e {
-// 			output = append(output, v...)
-// 		}
-// 	case []interface{}:
-// 		for _, v := range e {
-// 			output = append(output, FlattenFloat32(v)...)
-// 		}
-// 	case float32:
-// 		output = append(output, e)
-// 	default:
-// 		output = append(output, cast.ToFloat32(e))
-// 	}
-// 	return output
-// }
-
-func Flatten(f interface{}) []interface{} {
-
-	output := []interface{}{}
+func FlattenFloat32(f interface{}) ([]float32, error) {
+	output := []float32{}
 
 	switch e := f.(type) {
-	case []interface{}:
+	case [][]float32:
 		for _, v := range e {
-			output = append(output, Flatten(v)...)
+			output = append(output, v...)
 		}
-	case interface{}:
+	case []float32:
+		for _, v := range e {
+			fv, err := FlattenFloat32(v)
+			if err != nil {
+				return nil, err
+			}
+			output = append(output, fv...)
+		}
+	case float32:
 		output = append(output, e)
+	default:
+		return nil, errors.New("unable to cast interface to []float32")
 	}
-
-	return output
+	return output, nil
 }
 
 func Flatten2DFloat32(f interface{}) [][]float32 {
-
 	switch e := f.(type) {
 	case [][][][]float32:
 		output := [][]float32{}
@@ -103,4 +55,21 @@ func Flatten2DFloat32(f interface{}) [][]float32 {
 	}
 
 	return nil
+}
+
+// DOES NOT WORK
+func Flatten(f interface{}) []interface{} {
+
+	output := []interface{}{}
+
+	switch e := f.(type) {
+	case []interface{}:
+		for _, v := range e {
+			output = append(output, Flatten(v)...)
+		}
+	case interface{}:
+		output = append(output, e)
+	}
+
+	return output
 }
